3505b124ba19df90e0ac8aa8d50c067349b5d201
Maciej Sawka <maciejsawka@gmail.com>
2025-04-03 02:39:02 +0200

    Add blinking of led with hardware interrupt

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
 #include "stm32l476/registers.h"
 
+#define TIM7_IRQn (55)
+
 #define LED_PIN (5)
 
 void init_hw() {
     // First enable GPIOA peripheral
     RCC->AHB2ENR |= _BV(RCC_AHB2ENR_GPIOAEN);
+
+    // Also TIM7 peripheral
+    RCC->APB1ENR1 |= _BV(RCC_APB1ENR1_TIM7EN);
 
     // Good information on GPIO setup is in Reference manual 8.4, especially table 39.
     // Many of those operations are not needed due to default values of those registers,
@@ -27,20 +32,41 @@ void init_hw() {
     // Set pullups/pulldowns to no explicit choice
     GPIOA->OSPEEDR &= ~(0x3 << (LED_PIN * 2));
     GPIOA->OSPEEDR |= (GPIOx_OPUPD_NONE << (LED_PIN * 2));
+
+    // Set top value to 40000, so with 4MHz system clock it would results in 100Hz
+    TIM7->ARR = 40000;
+
+    // Set prescaler to 50 so it fires twice every second
+    // fCK_PSC / (PSC[15:0] + 1)
+    TIM7->PSC = 49;
+
+    // Enable settings
+    //TIM7->EGR |= _BV(TIMx_6_7_EGR_UG); // seems to not be needed? works without it
+
+    // Enable interrupt in NVIC too
+    NVIC_EnableIRQ(TIM7_IRQn);
+
+    // Enable interrupt in peripheral
+    TIM7->DIER |= _BV(TIMx_6_7_DIER_UIE);
+
+    // Finally enable clock
+    TIM7->CR1 |= _BV(TIMx_6_7_CR1_CEN);
+}
+
+// Interrupt handler for TIM6 overflow event
+// https://gcc.gnu.org/onlinedocs/gcc/ARM-Function-Attributes.html for attribute
+void TIM7_handler()  {
+    // flip output of the LED
+    GPIOA->ODR ^= (1 << LED_PIN);
+
+    // Clear interrupt flag
+    TIM7->SR &= ~(_BV(TIMx_6_7_SR_UIF));
 }
 
 int main(void) {
     init_hw();
 
-
-    int val = 0;
     while (1) {
-        if(val == 0) {
-            // Output high/low repeatedly to blink the led
-            // Due to clock speed, blinking is not visible, so it just pretty much lights it on
-            // TODO: blink the led using a timer mayhaps?
-            GPIOA->ODR ^= (1 << LED_PIN);
-        }
-        val += 1;
+        // Do nothing, should perhaps even sleep
     }
 }
--- stm32l476/registers.h
+++ stm32l476/registers.h
@@ -6,10 +6,39 @@
 // bit value macro
 #define _BV(N) (1 << (N))
 
+// ARM7m reference manual B3.4.3
+struct _NVIC_t_ {
+    uint32_t ISER[16];
+    // dont care about other stuff, maybe priority register could be useful in the future
+};
+
+// ARM7m reference manual B3.2
+#define NVIC ((volatile struct _NVIC_t_*) 0xE000E100)
+
+// Copied directly from https://github.com/ARM-software/CMSIS_4/blob/f2cad4345783c948ed4a7f5cdb02cdc0856366f1/CMSIS/Include/core_cm4.h#L1626C17-L1629C2
+// The arithmetic is easy but no reason to reinvent the wheel
+void NVIC_EnableIRQ(int32_t IRQn) {
+    NVIC->ISER[(((uint32_t)(int32_t)IRQn) >> 5UL)] = (uint32_t)(1UL << (((uint32_t)(int32_t)IRQn) & 0x1FUL));
+};
+
 struct _RCC_t {
-    uint8_t _dont_care[0x4C];
+    uint8_t _dont_care[0x08];
+
+    uint32_t CFGR; // clock configuration register - Reference manual 6.4.3 - offset 0x08, pins defined below
+    #define RCC_CFGR_HPRE0      4 // AHB prescaler bit 0
+    #define RCC_CFGR_HPRE1      5 // AHB prescaler bit 0
+    #define RCC_CFGR_HPRE2      6 // AHB prescaler bit 0
+    #define RCC_CFGR_HPRE3      7 // AHB prescaler bit 0
+
+    uint8_t _dont_care_2[0x40];
+
     uint32_t AHB2ENR; // Reference manual 6.4.17 - offset 0x4C, pins defined below
-#define RCC_AHB2ENR_GPIOAEN 0
+    #define RCC_AHB2ENR_GPIOAEN 0
+
+    uint8_t _dont_care_3[0x08];
+
+    uint32_t APB1ENR1; // Reference manual 6.4.19 - offset 0x58, pins defined below
+    #define RCC_APB1ENR1_TIM7EN 5
 };
 
 struct _GPIOx_t {
@@ -39,8 +68,42 @@ struct _GPIOx_t {
     // theres also BSRR and some other stuff i think but its not really needed for now?
 };
 
+// Structure only for TIM6 and TIM7! Reference manual 33.4
+// some of these registers are displayed as 16-bit in their manual entries, but in
+// 33.4.9 it is shown that they are indeed 32 bits, and their top half of bits just is not used
+struct _TIMx_6_7_t_{
+    uint32_t CR1; // control register 1 - Reference manual 33.4.1 - offset 0x0, bits defined below
+    #define TIMx_6_7_CR1_CEN    0 // Counter enable, 1 for on 0 for off (default off)
+    #define TIMx_6_7_CR1_UDIS   1 // Update disable, 1 for on (disabled update event) , 0 for off (event enabled)
+    #define TIMx_6_7_CR1_URS    2 // Update requst source - decides what generates an udpate event (Ref manual)
+    #define TIMx_6_7_CR1_OPM    3 // One pulse mode enable
+
+    uint8_t _dont_care[0x08];
+
+    uint32_t DIER; // DMA/interrupt enable register - Reference manual 33.4.3 - offset 0x0C, bits defined below
+    #define TIMx_6_7_DIER_UIE   0 // Interrupt enable
+    #define TIMx_6_7_DIER_UDE   8 // DMA request enable
+
+    uint32_t SR; // Status register - Reference manual 33.4.4 - offset 0x10, bits defined below
+    #define TIMx_6_7_SR_UIF     0 // Update interrupt flag - set to 1 by hardware on update event, has to be cleared in software
+
+    uint32_t EGR; // Event generation register - Reference manual 33.4.5 - offset 0x14, bits defined below
+    #define TIMx_6_7_EGR_UG     0 // Update generator - resets the timer and updates ARR and CNT values (?)
+
+    uint8_t _dont_care_2[0x10];
+
+    uint32_t PSC; // Prescaler register (16 bit) - Reference manual 33.4.7 - offset 0x28
+    // From the manual, counter clock frequency is equal to: fCK_PSC / (PSC[15:0] + 1)
+    // For example, with sysclk equal 4MHz, setting PSC to 39999 will result in frequency of
+    // 4MHz / 40000 = 100 Hz
+
+    uint32_t ARR; // Auto reload register (16 bit) - Reference manual 33.4.8 - offset 0x2C
+    // The value in the register specifies the top value for the counter to triggeri nterrupt on
+};
+
 // Below addresses from Datasheet 5. - Memory Map, table 19.
 #define RCC ((volatile struct _RCC_t*) 0x40021000)
 #define GPIOA ((volatile struct _GPIOx_t*) 0x48000000)
+#define TIM7 ((volatile struct _TIMx_6_7_t_*) 0x40001400)
 
 #endif
